c095e7f6f8d09ede83f431e55a9ad66156151ef1
songjj7253 <sjj10200104@dingtalk.com>
2021-04-27 16:03:24 +0800

    基础知识结束，开始进行项目开发

--- src/screens/project-list/index.jsx
+++
@@ -1,9 +0,0 @@
-import {SearchPanel} from './search-panel';
-import {List} from "./list"
-
-export const ProjectListScreen = () => {
-    return <div>
-        <SearchPanel />
-        <List />
-    </div>
-}
---
+++ src/utils/index.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => value === 0 ? false : !value
+
+export const cleanObject = (object: object) => {
+    const result = { ...object }
+    Object.keys(result).forEach(key => {
+        // @ts-ignore
+        const value = result[key]
+        if (isFalsy(value)) {
+            // @ts-ignore
+            delete result[key]
+        }
+    })
+    return result
+};
+
+export const useMount = (callback: () => void) => {
+    useEffect(() => {
+        callback()
+    }, [])
+}
+
+export const useDebounce = <V>(value: V, delay?: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebouncedValue(value), delay)
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+
+    return debouncedValue
+}
+
+export const useArray = <T>(initialValue: T[]) => {
+    const [value, setValue] = useState(initialValue)
+    return {
+        value,
+        setValue,
+        add: (item: T) => setValue([...value, item]),
+        clear: () => setValue([]),
+        removeIndex: (index: number) => {
+            let copy = [...value]
+            copy.splice(index, 1)
+            setValue(copy)
+        }
+    }
+}
