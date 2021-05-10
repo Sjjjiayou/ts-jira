18bebbb4ecac98f133dd9f5ded359cfd19aecc77
songjj7253 <sjj10200104@dingtalk.com>
2021-05-10 16:32:39 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { SearchPanel } from './search-panel';
 import { List } from "./list";
 import { cleanObject, useMount, useDebounce} from '../../utils';
-import * as qs from "qs"
-
-const apiUrl = process.env.REACT_APP_API_URL
+import { useHttp } from 'utils/http';
 
 export const ProjectListScreen = () => {
     const [param, setParam] = useState({
@@ -14,21 +12,14 @@ export const ProjectListScreen = () => {
     const [list, setList] = useState([])
     const [users, setUsers] = useState([])
     const debouncedParam = useDebounce(param, 200)
+    const client = useHttp()
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
-            if (response.ok) {
-                setList(await response.json())
-            }
-        })
+        client('projects', {data: cleanObject(debouncedParam)}).then(setList)
     }, [debouncedParam])
 
     useMount(() => {
-        fetch(`${apiUrl}/users`).then(async response => {
-            if (response.ok) {
-                setUsers(await response.json())
-            }
-        })
+        client('users').then(setUsers)
     })
 
     return <div>
