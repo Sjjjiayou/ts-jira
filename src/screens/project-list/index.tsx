d1b05eaafbda254d2d0d27ee33ac66a4d0b51473
songjj7253 <sjj10200104@dingtalk.com>
2021-05-13 11:33:53 +0800

    feat: 优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import { SearchPanel } from './search-panel';
 import { List } from "./list";
 import { cleanObject, useMount, useDebounce} from '../../utils';
 import { useHttp } from 'utils/http';
+import styled from '@emotion/styled';
 
 export const ProjectListScreen = () => {
     const [param, setParam] = useState({
@@ -22,8 +23,13 @@ export const ProjectListScreen = () => {
         client('users').then(setUsers)
     })
 
-    return <div>
+    return <Container>
+        <h1>项目列表</h1>
         <SearchPanel param={param} setParam={setParam} users={users} />
         <List list={list} users={users} />
-    </div>
+    </Container>
 }
+
+const Container = styled.div`
+padding: 3.2rem
+`
