e029170f6c11120edf37aac6944ba2af4422223c
songjj7253 <sjj10200104@dingtalk.com>
2021-05-24 19:08:39 +0800

    fix: 路由安装

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,42 +1,55 @@
 import React from 'react'
 import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screens/project-list'
+import {ProjectScreen} from 'screens/project'
 import styled from '@emotion/styled'
 import { Row } from "compontents/lib"
 import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg'
-import { Button, Dropdown, Menu } from 'antd'
+import { Button, Dropdown, Menu } from 'antd';
 import { useDocumentTitlt } from 'utils'
+import {Navigate, Route, Router, Routes } from 'react-router'
 
 export const AuthenticatedApp = () => {
-    const { logout, user } = useAuth()
-    useDocumentTitlt("项目列表" ,false)
+    useDocumentTitlt("项目列表", false)
     return (
         <Container>
-            <Header between={true}>
-                <HeaderLeft gap={true}>
-                    <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
-                    <h2>项目</h2>
-                    <h2>用户</h2>
-                </HeaderLeft>
-                <HeaderRight>
-                    <Dropdown overlay={<Menu>
-                        <Menu.Item key={'logout'}>
-                            <Button onClick={logout} type={'link'}>登出</Button>
-                        </Menu.Item>
-                    </Menu>
-                    }>
-                        <Button type={'link'} onClick={e => e.preventDefault()}>
-                            Hi, {user?.name}
-                        </Button>
-                    </Dropdown>
-                </HeaderRight>
-            </Header>
+            <PageHeader />
             <Main>
                 <ProjectListScreen />
+                <Routes>
+                    <Route path={'/projects'} element={<ProjectListScreen />}></Route>
+                    <Route path={'/projects/:projectId/*'} element={<ProjectScreen />}></Route>
+                </Routes>
             </Main>
         </Container>
     )
 }
+
+const PageHeader = () => {
+    const { logout, user } = useAuth()
+    return (
+        <Header between={true}>
+            <HeaderLeft gap={true}>
+                <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
+                <h2>项目</h2>
+                <h2>用户</h2>
+            </HeaderLeft>
+            <HeaderRight>
+                <Dropdown overlay={<Menu>
+                    <Menu.Item key={'logout'}>
+                        <Button onClick={logout} type={'link'}>登出</Button>
+                    </Menu.Item>
+                </Menu>
+                }>
+                    <Button type={'link'} onClick={e => e.preventDefault()}>
+                        Hi, {user?.name}
+                    </Button>
+                </Dropdown>
+            </HeaderRight>
+        </Header>
+    )
+}
+
 
 const Container = styled.div`
     display: grid;
