c1857048e830db9ff5fb4c5cc4dd1449940f0da5
sjjjiayou <18016457253@163.com>
2021-05-26 23:44:19 +0800

    feat: 看板和epic添加

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,69 +1,74 @@
-import React from 'react'
-import { useAuth } from 'context/auth-context'
-import { ProjectListScreen } from 'screens/project-list'
-import { ProjectScreen } from 'screens/project'
-import styled from '@emotion/styled'
-import { Row } from "compontents/lib"
-import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg'
-import { Button, Dropdown, Menu } from 'antd';
-import { useDocumentTitlt } from 'utils'
-import { Navigate, Route, Routes} from 'react-router'
-import { BrowserRouter as Router} from 'react-router-dom'
+import React from "react";
+import { useAuth } from "context/auth-context";
+import { ProjectListScreen } from "screens/project-list";
+import { ProjectScreen } from "screens/project";
+import styled from "@emotion/styled";
+import { Row } from "compontents/lib";
+import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
+import { Button, Dropdown, Menu } from "antd";
+import { resetRoute, useDocumentTitlt } from "utils";
+import { Navigate, Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const AuthenticatedApp = () => {
-    useDocumentTitlt("项目列表", false)
-    return (
-        <Container>
-            <PageHeader />
-            <Main>
-                    <Routes>
-                        <Route path={"/projects"} element={<ProjectListScreen />} />
-                        <Route path={"/projects/:projectId/*"} element={<ProjectScreen />} />
-                        <Navigate to={"/projects"}/>
-                    </Routes>
-            </Main>
-        </Container>
-    )
-}
+  useDocumentTitlt("项目列表", false);
+  return (
+    <Container>
+      <PageHeader />
+      <Main>
+        <Routes>
+          <Route path={"/projects"} element={<ProjectListScreen />} />
+          <Route path={"/projects/:projectId/*"} element={<ProjectScreen />} />
+          <Navigate to={"/projects"} />
+        </Routes>
+      </Main>
+    </Container>
+  );
+};
 
 const PageHeader = () => {
-    const { logout, user } = useAuth()
-    return (
-        <Header between={true}>
-            <HeaderLeft gap={true}>
-                <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
-                <h2>项目</h2>
-                <h2>用户</h2>
-            </HeaderLeft>
-            <HeaderRight>
-                <Dropdown overlay={<Menu>
-                    <Menu.Item key={'logout'}>
-                        <Button onClick={logout} type={'link'}>登出</Button>
-                    </Menu.Item>
-                </Menu>
-                }>
-                    <Button type={'link'} onClick={e => e.preventDefault()}>
-                        Hi, {user?.name}
-                    </Button>
-                </Dropdown>
-            </HeaderRight>
-        </Header>
-    )
-}
-
+  const { logout, user } = useAuth();
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <Button type={'link'} onClick={resetRoute}>
+          <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
+        </Button>
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={"logout"}>
+                <Button onClick={logout} type={"link"}>
+                  登出
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type={"link"} onClick={(e) => e.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
+  );
+};
 
 const Container = styled.div`
-    display: grid;
-    grid-template-rows: 6rem 1fr;
-    height: 100vh;
-`
-const Header = styled(Row)`
-    padding: 3.2rem;
-    box-shadow: 0 0 5px 0 rgba(0,0,0,0.1);
-    z-index: 1;
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
 `;
-const HeaderLeft = styled(Row)``
-const HeaderRight = styled.div``
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
 
-const Main = styled.main`
-`
+const Main = styled.main``;
