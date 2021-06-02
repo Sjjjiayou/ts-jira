b0f1380af4908b029f8be570b54c02447927dbaf
sjjjiayou <18016457253@163.com>
2021-06-02 17:04:37 +0800

    feat: 添加id-select.tsx

---
+++ src/utils/url.ts
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { useSearchParams, URLSearchParamsInit} from "react-router-dom";
+import { cleanObject } from "utils";
+
+// 返回页面url，指定键的参数值,
+export const useUrlQueryParam = <K extends string>(keys: K[]) => {
+  const [searchParams, setSearchParam] = useSearchParams();
+  return [
+    useMemo(
+      () =>
+        keys.reduce((prev: { [key in K]: string }, key: K) => {
+          return { ...prev, [key]: searchParams.get(key) || "" };
+        }, {} as { [key in K]: string }),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      [searchParams]
+    ),
+    (params: Partial<{[key in K]: unknown}>) => {
+      const o = cleanObject({...Object.fromEntries(searchParams), ...params}) as URLSearchParamsInit
+      return setSearchParam(o)
+    }
+  ] as const; 
+};
